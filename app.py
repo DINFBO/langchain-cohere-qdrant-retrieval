a70ec13327378e4ee9ca1d1acef7ae880ebe1561
JH.SUNG <86407466+DINFBO@users.noreply.github.com>
2023-06-09 14:08:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def retrieve_info():
     chain = load_qa_chain(OpenAI(openai_api_key=openai_api_key,temperature=0.2), chain_type="stuff")
     results = chain({"input_documents": search_results, "question": query}, return_only_outputs=True)
     
-    return {"results":search_results}
+    return {"results":results["output_text"], "te" : search_results[0].page_content }
